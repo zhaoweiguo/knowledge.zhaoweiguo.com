6ef33c8a98d132a241dc132e7a8cbcd5a9d12231
zhaoweiguo <zhaoweiguo@github.com>
2022-09-14 13:11:31 +0000

    [github workflow knowledge]Update changes

--- build/html/x-soft_design/_static/documentation_options.js
+++ build/html/x-soft_design/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'V1.6.16',
+    VERSION: 'V1.6.17',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
