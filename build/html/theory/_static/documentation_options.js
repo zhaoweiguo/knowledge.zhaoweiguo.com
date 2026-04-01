f1950fe5b1de288960b70971dc0073f14e4740bc
zhaoweiguo <zhaoweiguo@github.com>
2026-04-01 04:32:04 +0000

    [github workflow knowledge]Update changes

--- build/html/theory/_static/documentation_options.js
+++ build/html/theory/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'V2026.02',
+    VERSION: 'V2026.03',
     LANGUAGE: 'zh-CN',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
